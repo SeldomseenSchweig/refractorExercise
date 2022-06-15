ead3f10ef30edc52069e48c8ebb86bbafeeaa02e
SeldomseenSchweig <jamesschweig@gmail.com>
2022-06-15 11:32:29 -0700

    first commit

---
+++ refractor.js
@@ -0,0 +1,32 @@
+// function double(arr) {
+//     return arr.map(function(val) {
+//         return val * 2;
+//       });
+//     }
+
+
+
+ 
+const double = (arr) => arr.map(val => val *2);
+
+
+
+// function (numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+
+const squareAndFindEvens = (numbers) => 
+    // let squares = numbers.map(num => num **2);
+    // let evens = squares.filter(square=> square % 2 ===0)
+     evens = numbers.map(num => num **2).filter(num => num % 2 ===0);
+
+
+    
+
+ 
